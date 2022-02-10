cf1ba45d3f0fe72b01a5daeb1549941d67488d1f
Miko <miko.palojarvi@student.oulu.fi>
2022-02-10 17:25:40 +0200

    formatting, renaming

--- backend/src/app.py
+++ backend/src/app.py
@@ -1,10 +1,13 @@
 from flask import Flask, Response
+from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 
 from config import DevelopmentConfig, ProductionConfig
-from models import PlayedGame, Location, User
 
 app = Flask(__name__)
 app.config.from_object(DevelopmentConfig)
 db = SQLAlchemy(app)
 
+import models
+
+migrate = Migrate(app, db)
--- backend/src/models.py
+++ backend/src/models.py
@@ -2,18 +2,18 @@ from app import db
 
 class PlayedGame(db.Model):
     id = db.Column(db.Integer, primary_key=True) # Check this https://docs.sqlalchemy.org/en/14/dialects/postgresql.html#sequences-serial-identity
-    playerName = db.Column(db.String(3), nullable=False)
+    player_name = db.Column(db.String(3), nullable=False)
     score = db.Column(db.Integer, nullable=False)
-    timeStamp = db.Column(db.DateTime, nullable=False)
-    gameType = db.Column(db.Integer)
+    timestamp = db.Column(db.DateTime, nullable=False)
+    game_type = db.Column(db.Integer)
 
 class Location(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    imagePath = db.Column(db.String(64), nullable=False)
-    countryName = db.Column(db.String(64), nullable=False)
-    townName = db.Column(db.String(64), nullable=False)
+    image_path = db.Column(db.String(64), nullable=False)
+    country_name = db.Column(db.String(64), nullable=False)
+    town_name = db.Column(db.String(64), nullable=False)
 
-class User(db.Model):
+class Person(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(64), nullable=False)
     password = db.Column(db.String(64), nullable=False)
