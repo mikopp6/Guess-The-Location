e2c4edf414c0b1e0ac86a1ce9c32f5936c197654
Santeri Yritys <p1764@student.jamk.fi>
2022-03-08 14:08:50 +0200

    fixed file structure

--- api/gtl/populate.py
+++ api/gtl/populate.py
@@ -10,9 +10,15 @@ app.app_context().push()
 
 # List of users/admins/whatever (person-table)
 persons = [
-  { "email": "thisismy@email.com", "password": "exampleofastrongpasswordthisisnt"},
-  { "email": "thisisnotmy@email.com", "password": "exampleofanevenstrongerpasswordthisisnt"},
-  { "email": "thisusedtobemy@email.com", "password": "exampleofthestrongestpasswordintheworldthisisnt"},
+    {"email": "thisismy@email.com", "password": "exampleofastrongpasswordthisisnt"},
+    {
+        "email": "thisisnotmy@email.com",
+        "password": "exampleofanevenstrongerpasswordthisisnt",
+    },
+    {
+        "email": "thisusedtobemy@email.com",
+        "password": "exampleofthestrongestpasswordintheworldthisisnt",
+    },
 ]
 # List names
 names = ["AAA", "BBB", "CCC", "DDD", "EEE", "FFF", "GGG", "HHH", "III", "JJJ"]
@@ -20,48 +26,48 @@ names = ["AAA", "BBB", "CCC", "DDD", "EEE", "FFF", "GGG", "HHH", "III", "JJJ"]
 games = []
 # Generate list of games from names
 for name in names:
-  games.append(
-    {
-      "player_name": name,
-      "score": randint(0, 10000),
-      "timestamp": datetime.datetime.utcnow(),
-      "game_type": randint(0, 3)
-    }
-  )
+    games.append(
+        {
+            "player_name": name,
+            "score": randint(0, 10000),
+            "timestamp": datetime.datetime.utcnow(),
+            "game_type": randint(0, 3),
+        }
+    )
 
 # Add persons to db session
 for person in persons:
-  pers = Person(
-      email = person["email"],
-      password = person["password"],
+    pers = Person(
+        email=person["email"],
+        password=person["password"],
     )
 
-  db.session.add(pers)
+    db.session.add(pers)
 
 # Add locations to db session
-with open('images/locationdata.json') as json_file:
+with open("static/locationdata.json") as json_file:
     data = json.load(json_file)
 
     for location in data["locations"]:
-      loc = Location(
-        image_path = location["image_path"],
-        country_name = location["country_name"],
-        town_name = location["town_name"],
-        person_id = location["person_id"]
-      )
-      
-      db.session.add(loc)
+        loc = Location(
+            image_path=location["image_path"],
+            country_name=location["country_name"],
+            town_name=location["town_name"],
+            person_id=location["person_id"],
+        )
+
+        db.session.add(loc)
 
 # Add played games to db session
 for game in games:
-  pg = PlayedGame(
-      player_name = game["player_name"],
-      score = game["score"],
-      timestamp = game["timestamp"],
-      game_type = game["game_type"],
+    pg = PlayedGame(
+        player_name=game["player_name"],
+        score=game["score"],
+        timestamp=game["timestamp"],
+        game_type=game["game_type"],
     )
 
-  db.session.add(pg)
+    db.session.add(pg)
 
 
 # Commit persons, locations and games
