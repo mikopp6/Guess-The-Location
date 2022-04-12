d144879ee7c126a0137af46abe68ece8bf98eb25
Miko <miko.palojarvi@student.oulu.fi>
2022-04-12 12:48:48 +0300

    made personservice

--- frontend/gtl/src/components/LogInCard.tsx
+++ frontend/gtl/src/components/LogInCard.tsx
@@ -1,18 +1,43 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Button from '@material-ui/core/Button';
 import TextField from '@material-ui/core/TextField';
 import Box from '@material-ui/core/Box';
 
+import IPerson from '../types/Person';
+import PersonService from '../services/PersonService';
+
 const LogInCard: React.FC = () => {
+  const [persons, setPersons] = useState<Array<IPerson>>([])
+  const [fetchIsDone, setFetchIsDone] = useState(false)
+
+  useEffect(() => {
+    retrievePersons()
+  }, [])
+
   const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
     const data = new FormData(event.currentTarget);
-    console.log({
-      email: data.get('email'),
-      password: data.get('password'),
-    });
+    const email = data.get('email')
+    const password = data.get('password')
+    console.log("Submitted: ", email, password);
+    for(const key in persons){
+      const person = persons[key]
+      if (person.email === email && person.password === password){
+        console.log("Success!")
+      }
+    }
   };
 
+  const retrievePersons = () => {
+    PersonService.getAll()
+      .then((response: any) => {
+        setPersons(response.data.items)
+        setFetchIsDone(true)
+      })
+      .catch((e: Error) => {
+        console.log(e)
+      })
+  }
   return (
     <Box component="form" onSubmit={handleSubmit} >
       <TextField
---
+++ frontend/gtl/src/services/PersonService.ts
@@ -0,0 +1,33 @@
+import http from "../http-common";
+import IPerson from "../types/Person";
+const getAll = () => {
+  return http.get<Array<IPerson>>("/persons/");
+};
+// const get = (id: any) => {
+//   return http.get<ILocation>(`/locations/${id}`);
+// };
+// const create = (data: ILocation) => {
+//   return http.post<ILocation>("/locations", data);
+// };
+// const update = (id: any, data: ILocation) => {
+//   return http.put<any>(`/locations/${id}`, data);
+// };
+// const remove = (id: any) => {
+//   return http.delete<any>(`/locations/${id}`);
+// };
+// const removeAll = () => {
+//   return http.delete<any>(`/locations`);
+// };
+// const findByTitle = (title: string) => {
+//   return http.get<Array<ILocation>>(`/locations?title=${title}`);
+// };
+const PersonService = {
+  getAll,
+  // get,
+  // create,
+  // update,
+  // remove,
+  // removeAll,
+  // findByTitle,
+};
+export default PersonService;
