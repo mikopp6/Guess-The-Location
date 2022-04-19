120bf542bf158c325a06a606c044dbb361e5a24a
Miko <miko.palojarvi@student.oulu.fi>
2022-04-19 20:30:57 +0300

    more addlocation stuff

--- frontend/gtl/src/components/AddLocationCard.tsx
+++ frontend/gtl/src/components/AddLocationCard.tsx
@@ -1,12 +1,8 @@
-import React, { useEffect, useState } from "react"
+import React from "react"
 import Button from "@material-ui/core/Button"
 import TextField from "@material-ui/core/TextField"
 import Box from "@material-ui/core/Box"
 
-import IPerson from "../types/Person"
-import PersonService from "../services/PersonService"
-import { useNavigate } from "react-router-dom"
-import { AxiosResponse } from "axios"
 
 /**
  * AddLocationCard
@@ -17,43 +13,12 @@ import { AxiosResponse } from "axios"
 
 
 const AddLocationCard: React.FC = () => {
-    const [persons, setPersons] = useState<Array<IPerson>>([])
-    const [fetchIsDone, setFetchIsDone] = useState(false)
-
-    const navigate = useNavigate()
-
-    useEffect(() => {
-        retrievePersons()
-    }, [])
-  
-    const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-        event.preventDefault()
-        const data = new FormData(event.currentTarget)
-        const email = data.get("email")
-        const password = data.get("password")
-        console.log("Submitted: ", email, password)
-        for(const key in persons){
-            const person = persons[key]
-            if (person.email === email && person.password === password){
-                if (fetchIsDone){
-                    navigate("/admin")
-                }
-            }
-        }
+    const handleSubmit = () => {
+        console.log("yes")
     }
 
-    const retrievePersons = () => {
-        PersonService.getAll()
-            .then((response: AxiosResponse) => {
-                setPersons(response.data.items)
-                setFetchIsDone(true)
-            })
-            .catch((e: Error) => {
-                console.log(e)
-            })
-    }
     return (
-        <Box component="form" onSubmit={handleSubmit} >
+        <Box sx={{ width: 300, height: 225 }}>
             <TextField 
                 type="file"
                 name="image"
@@ -75,7 +40,7 @@ const AddLocationCard: React.FC = () => {
                 label="Town"
                 name="Town"
             />
-            <Button type="submit" variant="outlined" >
+            <Button type="submit" variant="outlined" onClick={() => handleSubmit()}>
                 Submit
             </Button>
         </Box>
